5996d985a696d9b95f15f5ce66730b39f0a2cef8
Murilo Santos <mucrilobass@gmail.com>
2022-10-28 12:08:32 -0400

    Olá Mundo!

---
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(value="/")
+    public String getMethodName() {
+        
+        return "Olá Mundo VSCode!";
+    }
+}
